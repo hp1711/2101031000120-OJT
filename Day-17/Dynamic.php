42c1c3c0118d55a5d2d6751f86251758c1cf1be5
harsh panchal <82900127+hp1711@users.noreply.github.com>
2023-07-07 19:35:47 +0530

    PHP

---
+++ Day-17/Dynamic.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+$fname=$_REQUEST['firstname'];
+$lname=$_REQUEST['lastname'];
+$email=$_REQUEST['email'];
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('$fname', '$lname', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
